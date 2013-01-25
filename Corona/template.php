f209c1fc1d0316e6fa89ee9b24ed046f2674cea4
Ian Carrico <Ian@FourKitchens.com>
2013-01-24 18:04:38 -0600

    Add in corona, so they can see this later

---
+++ Corona/template.php
@@ -0,0 +1,187 @@
+<?php
+
+/**
+ * Override or insert variables into the maintenance page template.
+ *
+ * @param $vars
+ *   An array of variables to pass to the theme template.
+ * @param $hook
+ *   The name of the template being rendered ("maintenance_page" in this case.)
+ */
+function saros_preprocess_maintenance_page(&$vars, $hook) {
+  // When a variable is manipulated or added in preprocess_html or
+  // preprocess_page, that same work is probably needed for the maintenance page
+  // as well, so we can just re-use those functions to do that work here.
+  // saros_preprocess_html($variables, $hook);
+  // saros_preprocess_page($variables, $hook);
+
+  // This preprocessor will also be used if the db is inactive. To ensure your
+  // theme is used, add the following line to your settings.php file:
+  // $conf['maintenance_theme'] = 'saros';
+  // Also, check $vars['db_is_active'] before doing any db queries.
+}
+
+/**
+ * Implements hook_modernizr_load_alter().
+ *
+ * @return
+ *   An array to be output as yepnope testObjects.
+ */
+function saros_modernizr_load_alter(&$load) {
+
+  // We will check for touch events, and if we do load the hammer.js script.
+  $load[] = array(
+    'test' => 'Modernizr.touch',
+    'yep'  => array('/'. drupal_get_path('theme','saros') . '/js/hammer.js'),
+  );
+
+  return $load;
+}
+
+/**
+ * Implements hook_preprocess_html()
+ *
+ * @param $vars
+ *   An array of variables to pass to the theme template.
+ * @param $hook
+ *   The name of the template being rendered ("html" in this case.)
+ */
+function saros_preprocess_html(&$vars) {
+
+  drupal_add_css('http://fonts.googleapis.com/css?family=Inder|Duru+Sans', array('type' => 'external', 'group' => CSS_SYSTEM, 'every_page' => TRUE));
+
+}
+
+/**
+ * Override or insert variables into the page template.
+ *
+ * @param $vars
+ *   An array of variables to pass to the theme template.
+ * @param $hook
+ *   The name of the template being rendered ("page" in this case.)
+ */
+/* -- Delete this line if you want to use this function
+function saros_preprocess_page(&$vars) {
+
+}
+
+/**
+ * Override or insert variables into the region templates.
+ *
+ * @param $vars
+ *   An array of variables to pass to the theme template.
+ * @param $hook
+ *   The name of the template being rendered ("region" in this case.)
+ */
+/* -- Delete this line if you want to use this function
+function saros_preprocess_region(&$vars, $hook) {
+
+}
+// */
+
+/**
+ * Override or insert variables into the block templates.
+ *
+ * @param $vars
+ *   An array of variables to pass to the theme template.
+ * @param $hook
+ *   The name of the template being rendered ("block" in this case.)
+ */
+/* -- Delete this line if you want to use this function
+function saros_preprocess_block(&$vars, $hook) {
+
+}
+// */
+
+/**
+ * Override or insert variables into the entity template.
+ *
+ * @param $vars
+ *   An array of variables to pass to the theme template.
+ * @param $hook
+ *   The name of the template being rendered ("entity" in this case.)
+ */
+/* -- Delete this line if you want to use this function
+function saros_preprocess_entity(&$vars, $hook) {
+
+}
+// */
+
+/**
+ * Override or insert variables into the node template.
+ *
+ * @param $vars
+ *   An array of variables to pass to the theme template.
+ * @param $hook
+ *   The name of the template being rendered ("node" in this case.)
+ */
+/* -- Delete this line if you want to use this function
+function saros_preprocess_node(&$vars, $hook) {
+  $node = $vars['node'];
+}
+// */
+
+/**
+ * Override or insert variables into the field template.
+ *
+ * @param $vars
+ *   An array of variables to pass to the theme template.
+ * @param $hook
+ *   The name of the template being rendered ("field" in this case.)
+ */
+/* -- Delete this line if you want to use this function
+function saros_preprocess_field(&$vars, $hook) {
+
+}
+// */
+
+/**
+ * Override or insert variables into the comment template.
+ *
+ * @param $vars
+ *   An array of variables to pass to the theme template.
+ * @param $hook
+ *   The name of the template being rendered ("comment" in this case.)
+ */
+/* -- Delete this line if you want to use this function
+function saros_preprocess_comment(&$vars, $hook) {
+  $comment = $vars['comment'];
+}
+// */
+
+/**
+ * Override or insert variables into the views template.
+ *
+ * @param $vars
+ *   An array of variables to pass to the theme template.
+ */
+/* -- Delete this line if you want to use this function
+function saros_preprocess_views_view(&$vars) {
+  $view = $vars['view'];
+}
+// */
+
+
+/**
+ * Override or insert css on the site.
+ *
+ * @param $css
+ *   An array of all CSS items being requested on the page.
+ */
+/* -- Delete this line if you want to use this function
+function saros_css_alter(&$css) {
+
+}
+// */
+
+/**
+ * Override or insert javascript on the site.
+ *
+ * @param $js
+ *   An array of all JavaScript being presented on the page.
+ */
+/* -- Delete this line if you want to use this function
+function saros_js_alter(&$js) {
+
+}
+// */
